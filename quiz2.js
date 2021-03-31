b09c1dedf6b2180c5a801b0959170a45e90740f3
ChiaChi-Hsu <plus7kiki926@gmail.com>
2021-03-31 17:40:12 +0800

    quiz2

---
+++ quiz2.js
@@ -0,0 +1,16 @@
+var tags = "cat";
+var dataUrl = "https://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&per_page=400&jsoncallback=?";
+
+var data = $.getJSON(dataUrl);
+
+data.done(function (msg) {
+$.each(msg.items, function(i, item){
+$("#contain").html();
+$("#contain").append($("<img/>").attr("src", item.media.m));
+});
+});
+
+data.fail(function (msg){
+console.log(msg);
+$("#contain").html("oops...fail getting data");
+});
